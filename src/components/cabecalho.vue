7049510ef6f44b3e22fdb15fa309832ad81a42c3
Leonardo Maros <marosleo1990@gmail.com>
2021-09-16 10:51:33 -0300

    first commit

---
+++ src/components/cabecalho.vue
@@ -0,0 +1,30 @@
+<template>
+  <header>
+      <h1>Lista de tarefas</h1>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+header {
+  background-color: rgb(98, 0, 163);
+  color: rgb(251, 255, 0); text-shadow: black 0.1em 0.1em 0.2em;
+  height: 60px;
+  width: 100%;
+  position: fixed;
+  top: 0;
+
+
+}
+
+h1 {
+  font-family:Arial;
+  text-align: center;
+}
+</style>
+
